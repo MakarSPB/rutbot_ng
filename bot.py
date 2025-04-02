febaf2f737d83f7c0c61411419c8ae5beea65843
Makar-aka <moptuh@yandex.ru>
2025-04-03 02:41:47 +0300

    Добавлена кнопка отмены и улучшена обработка ошибок
    В функции `get_url_command` добавлена возможность отмены загрузки с помощью кнопки "Отмена". Пользователь теперь видит сообщение о том, что он может отменить действие.
    
    Также в блоке `if __name__ == "__main__":` добавлен параметр `none_stop=True`, что позволяет боту продолжать работу даже в случае возникновения ошибок.

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ def get_url_command(message):
     # Добавление кнопки "Отмена"
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton("Отмена", callback_data="cancel_search"))
-    bot.send_message(message.chat.id, reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Вы можете отменить загрузку, нажав кнопку ниже.", reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: call.data == "search_prompt")
 def search_prompt(call):
@@ -318,6 +318,7 @@ def cancel_search(call):
 if __name__ == "__main__":
     logging.info("Бот запущен")
     bot.polling(none_stop=True)
+
 
 
 
