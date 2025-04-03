afead6b07e5f975c4ac2d9c64b8c401d4bd91faa
Makar-aka <moptuh@yandex.ru>
2025-04-03 04:04:40 +0300

    Обновлено сообщение уведомления для подписчиков
    Изменено текст уведомления в функции `notify_subscribers`. Теперь вместо "Файл BASE_FILE был обновлён. Добавлены:" используется "Обновления в базе!\nДобавлены:". Это улучшает формулировку уведомления о добавленных обновлениях.

--- bot.py
+++ bot.py
@@ -364,7 +364,7 @@ def check_base_file_updates():
 def notify_subscribers(new_lines):
     new_lines_text = ''.join(new_lines)
     for chat_id in subscribers:
-        bot.send_message(chat_id, f"Файл BASE_FILE был обновлён. Добавлены:\n{new_lines_text}")
+        bot.send_message(chat_id, f"Обновления в базе!\nДобавлены:\n{new_lines_text}")
 
 # Запуск проверки обновлений в отдельном потоке
 update_thread = Thread(target=check_base_file_updates)
